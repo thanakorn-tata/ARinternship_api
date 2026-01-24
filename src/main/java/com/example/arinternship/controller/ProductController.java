598fd129c6308695646b92f1a0ab79e65fa1553a
papawin <frame2346@gmail.com>
2026-01-24 18:31:20 +0700

    login

---
+++ src/main/java/com/example/arinternship/controller/ProductController.java
@@ -0,0 +1,28 @@
+package com.example.arinternship.controller;
+
+import com.example.arinternship.entity.Product;
+import com.example.arinternship.repository.ProductRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final ProductRepository repository;
+
+    public ProductController(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public List<Product> getAll() {
+        return repository.findAll();
+    }
+
+    @PostMapping
+    public Product create(@RequestBody Product product) {
+        return repository.save(product);
+    }
+}
